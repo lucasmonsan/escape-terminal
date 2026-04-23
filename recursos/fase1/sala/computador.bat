53bb6c55471a520a8dfd7f9bfe48f14f1ad376ed
Lucas Monsan <75125049+lucasmonsan@users.noreply.github.com>
2026-04-22 22:21:37 -0300

    Update computador.bat

--- recursos/fase1/sala/computador.bat
+++ recursos/fase1/sala/computador.bat
@@ -1,12 +1,32 @@
 @echo off
-chcp 65001 >nul
-echo ======================================================
-echo                   TERMINAL DE ACESSO
-echo ======================================================
+chcp 850 >nul
+
+:: --- VERIFICACAO ANTI-CHEAT ---
+tasklist /fi "imagename eq explorer.exe" | find /i "explorer.exe" >nul
+if %errorlevel% equ 0 (
+    cls
+    echo #########################################################
+    echo #           GAME OVER: INTERFACE DETECTADA              #
+    echo #########################################################
+    echo.
+    echo O jogo foi feito para usar o CMD
+    echo e voce esta usando a pasta visual.
+    echo.
+    echo Seu acesso foi negado.
+    echo.
+    echo Reinicie o jogo.
+    echo #########################################################
+    timeout /t 5 >nul
+    exit
+)
+
+echo =========================================================
+echo                    TERMINAL DE ACESSO
+echo =========================================================
 echo.
 timeout /t 1 >nul
 
-:: Verifica se o pendrive está na mochila dentro da sala
+:: Verifica se o pendrive esta na mochila dentro da sala
 if exist "mochila\pendrive.sys" (
     
     echo Verificando unidade externa...
@@ -20,12 +40,13 @@ if exist "mochila\pendrive.sys" (
     attrib -h "computador" /s /d >nul 2>&1
     echo Sistema carregado com sucesso! Explore a pasta 'computador'.
     
+    echo.
     timeout /t 1 >nul
-    echo Este terminal de instalação será removido.
+    echo Este terminal de instalacao sera removido.
     pause
     del "%~f0"
 ) else (
-    echo [ERRO]: Disco de boot não encontrado.
+    echo [ERRO]: Disco de boot nao encontrado.
     timeout /t 1 >nul
     echo Insira o pendrive na mochila e tente novamente.
     pause
@@ -33,5 +54,5 @@ if exist "mochila\pendrive.sys" (
 exit /b
 
 :FALHA
-echo [ERRO]: Dados do pendrive corrompidos ou ilegíveis.
+echo [ERRO]: Dados do pendrive corrompidos ou ilegiveis.
 pause
